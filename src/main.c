4fa2623ce0e6137aedd2465444ed1ed4053cdf7a
Antonio Rosales <jose.rosales.mald@gmail.com>
2023-11-04 20:27:19 -0600

    refactor: handler in different file

--- src/main.c
+++ src/main.c
@@ -1,21 +1,7 @@
 #include <microhttpd.h>
 #include <stdio.h>
-#include <string.h>
+#include "handler.h"
 
-enum MHD_Result request_handler(void *cls, struct MHD_Connection *connection,
-                                const char *url, const char *method,
-                                const char *version, const char *upload_data,
-                                size_t *upload_data_size, void **con_cls) {
-  const char *page = "<html><body>Hello, World!</body></html>";
-  struct MHD_Response *response;
-
-  response = MHD_create_response_from_buffer(strlen(page), (void *)page,
-                                             MHD_RESPMEM_PERSISTENT);
-  int ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
-  MHD_destroy_response(response);
-
-  return ret;
-}
 
 int main(int argc, char const *argv[]) {
   struct MHD_Daemon *daemon;
