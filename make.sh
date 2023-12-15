08fc4337ed4b1f0f33672611b5fac824df881c95
Antonio Rosales <jose.rosales.mald@gmail.com>
2023-12-14 22:18:15 -0600

    idk

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ MAIN_SOURCE="src/main.c"
 HANDLER_SOURCE="src/handler.c"
 
 # Libraries parameters
-COMPILE_OPTIONS="-lmicrohttpd -lhttp_parser"
+COMPILE_OPTIONS="-I/usr/include/libbson-1.0 -I/usr/include/libmongoc-1.0 -L/usr/lib64 -lbson-1.0 -lmongoc-1.0 -lcjson -lmicrohttpd -pthread -lhttp_parser"
 
 # Compilation command
 gcc -o $OUTPUT_PROGRAM $MAIN_SOURCE $HANDLER_SOURCE $COMPILE_OPTIONS
